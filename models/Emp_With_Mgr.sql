6bc8a072915ca2b1371af38a178c223c784a4d05
Sahaya Prabhu <jcsahaya2006@gmail.com>
2025-06-23 06:43:57 +0000

    Filter_Emp_with_Mgr

---
+++ models/Emp_With_Mgr.sql
@@ -0,0 +1,12 @@
+{{
+    config(
+        materialized='table',
+        transient=false
+    )
+}}
+
+select e.employee_id, concat(e.first_name||' '||e.last_name) as Full_Name, 
+e.salary, e.department_id, e.manager_id, concat(e1.first_name||' '||e1.last_name) as Manager_Name 
+from employees e
+join employees e1 
+on e.manager_id = e1.employee_id
